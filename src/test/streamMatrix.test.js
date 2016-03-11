ba1db64e5ee1261d403e34edfc0087d5fdbc8ab7
Ollie Edwards <ollie@brandwatch.com>
2016-03-11 10:37:24 +0000

    building up a collection of more formalised stream logic

--- src/test/streamMatrix.test.js
+++ src/test/streamMatrix.test.js
@@ -2,22 +2,39 @@
 
 const test = require('tape');
 const streamMatrix = require('streamMatrix');
-console.log(streamMatrix);
 const from = require('from2');
 const through = require('through2');
+const stream = require('stream');
 
 const objects = [{
     foo: 'bar'
 }];
 
-const source = from.obj(objects);
-const dest = through.obj();
-
 test('it should assemble a pipeline of streams from an array of streams', t => {
     t.plan(1);
-    streamMatrix.row([source, through.obj(), through.obj(), dest]).on('data', d => {
+    streamMatrix.row([from.obj(objects), through.obj(), through.obj(), through.obj()]).on('data', d => {
         t.deepEquals(d, objects[0]);
         t.end();
     });
 });
 
+test('it should fill in blank cells with through streams', t => {
+    streamMatrix.row([from.obj(objects), undefined, false, through.obj()]).on('data', d=> {
+        t.deepEquals(d, objects[0]);
+        t.end();
+    });
+});
+
+test('it should filter arbitrary objects from matrix', t => {
+    const result = streamMatrix.filter([[1,2,3],[4,5,6]], n => n%2);
+    t.deepEquals(result, [[1,3],[5]]);
+    t.end();
+});
+
+test('it should filter transforms from matrix', t=> {
+    const source = from.obj(objects);
+    const thru = through.obj();
+    const dest = stream.Writable();
+    t.deepEquals(streamMatrix.transforms([[source, thru, dest]]), [[thru]]);
+    t.end();
+});
---
+++ src/test/throttle.test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const test = require('tape');
+const through = require('through2');
+const from = require('from2');
+const throttle = require('throttle').throttle;
+
+test('it should delay writes on a stream', t => {
+
+    const vals = ['get this after 1 tick', '2 ticks', '3 ticks'];
+    t.plan(vals.length);
+    const delay = {
+        interval: () => 0
+    };
+
+    from(vals).pipe(throttle(through(), delay)).on('data', d => {
+        t.equals(d.toString(), vals.shift());
+    }).on('end', t.end);
+});
